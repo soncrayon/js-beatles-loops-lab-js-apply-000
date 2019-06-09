245e4b9a7c9d73584dd7b0a02649d0f6d727af6a
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-09 00:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+let emptyArray = [];
+let factsArray = []; 
+
+function theBeatlesPlay (musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray; 
+}
+
+function johnLennonFacts (facts) {
+  for (let i = 0; i < facts.length; i++) {
+    factsArray.push(facts[i] + "!!!"); 
+  }
+  return factsArray; 
+}
+
+
+function iLoveTheBeatles (n) {
+  let loveArray = [];
+  let text = "I love the Beatles!";
+  let i = 0; 
+  do {
+    loveArray.push(text); 
+    n++; 
+  }
+  while (i < 15);
+  return loveArray; 
+}
